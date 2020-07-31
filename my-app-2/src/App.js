c770bf8d053725e6d2ca520db722924ae27eee3f
Tiến Đạt <ntd.k108cva@gmail.com>
2020-07-31 16:56:22 +0700

    update

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import News from "./components/News";
 import { connect } from "react-redux";
@@ -9,9 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
         {this.props.num}
         <News />
       </div>
