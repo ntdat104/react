60959fa8cdb4bc9a39951f7a7ba3bc200e8410cc
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-09-25 08:25:06 +0700

    update

--- react-basic/src/Lession/Lession5.js
+++ react-basic/src/Lession/Lession5.js
@@ -6,8 +6,8 @@ class Lession5 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: "",
+      userName: "",
+      passWord: "",
       textDesc: "",
       gender: 0,
       lang: "vi",
@@ -35,62 +35,23 @@ class Lession5 extends Component {
       <div className="lession5">
         <h1>Form nhập liệu</h1>
         <form onSubmit={(e) => this.handleSubmit(e)}>
-          <input
-            name="username"
-            type="text"
-            placeholder="Username"
-            onChange={(e) => this.handleChange(e)}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            onChange={(e) => this.handleChange(e)}
-          />
-          <textarea
-            name="textDesc"
-            placeholder="Mô tả"
-            onChange={(e) => this.handleChange(e)}
-          ></textarea>
-          <select
-            name="gender"
-            value={this.state.gender}
-            onChange={(e) => this.handleChange(e)}
-          >
+          <input name="userName" type="text" placeholder="Username" onChange={(e) => this.handleChange(e)}/>
+          <input name="passWord" type="password" placeholder="Password" onChange={(e) => this.handleChange(e)}/>
+          <textarea name="textDesc" placeholder="Mô tả" onChange={(e) => this.handleChange(e)}></textarea>
+          <select name="gender" defaultValue={this.state.gender} onChange={(e) => this.handleChange(e)}>
             <option value={0}>Male</option>
             <option value={1}>Female</option>
           </select>
           <div className="radio">
-            <input
-              type="radio"
-              name="lang"
-              value="vi"
-              id="vi"
-              onChange={(e) => this.handleChange(e)}
-              checked={this.state.lang === "vi"}
-            />
+            <input type="radio" name="lang" value="vi" id="vi" onChange={(e) => this.handleChange(e)} checked={this.state.lang === "vi"}/>
             <label htmlFor="vi">Vietnamese</label>
           </div>
           <div className="radio">
-            <input
-              type="radio"
-              name="lang"
-              value="en"
-              id="en"
-              onChange={(e) => this.handleChange(e)}
-              checked={this.state.lang === "en"}
-            />
+            <input type="radio" name="lang" value="en" id="en" onChange={(e) => this.handleChange(e)} checked={this.state.lang === "en"}/>
             <label htmlFor="en">English</label>
           </div>
           <div className="checkbox">
-            <input
-              type="checkbox"
-              name="checkRobot"
-              value={true}
-              id="checkbox"
-              onChange={(e) => this.handleChange(e)}
-              checked={this.state.checkRobot === true}
-            />
+            <input type="checkbox" name="checkRobot" defaultValue ={true} id="checkbox" onChange={(e) => this.handleChange(e)} checked={this.state.checkRobot === true}/>
             <label htmlFor="checkbox">Tôi không phải người máy</label>
           </div>
           <div className="btn">
